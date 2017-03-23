c8b6dffada2ca1bc0229e0fc700101d970868e43
LFOLMERT <lukasfolmert@gmail.com>
2017-03-23 12:40:14 +0100

    updated README.md and options example in JSDoc

--- README.md
+++ README.md
@@ -34,8 +34,8 @@ $('#sortiBoxesContainer').sortiBoxes(options)
 var options = {
         params: [
             {
-                name:  'foo',
-                label: 'Foo'
+                name:  'foo', // used for matching element values
+                label: 'Foo' // used for displaying in <thead> of all boxes
             },
             {
                 name:  'bar',
@@ -45,23 +45,24 @@ var options = {
         data:   [
             {
                 id:   1,
-                name: 'Section name',
+                name: 'Section name', // will appear as <h5> header
 
                 boxes: [{
                     id:   1,
-                    name: 'Box name', // will be used as header
+                    name: 'Box name', // will appear as box header
 
                     elements: [
                         {
                             id:       1,
-                            name:     'Element A',
+                            name:     'Element A', // will appear under Name column
                             special:  true, // will add red dot
                             selected: true, // makes element visible in selected area
-                            status:   true, // TODO: add new class when element.status == true
+                            status:   true, // not used yet, TODO: add new class when element.status == true
                             params: {
                                 // keys must match names of main params
                                 foo: 7,
-                                bar: 42
+                                // accepts HTML too:                                
+                                bar: '<span><input type="text" name="box[1][element][1][bar]" value="9"></span>'
                             }
                         },
                         {
@@ -72,26 +73,30 @@ var options = {
                             status:   true,
                             params: {
                                 foo: 33,
-                                bar: 100
+                                bar: '<span><input type="text" name="box[1][element][2][bar]" value="46"></span>'                                
                             }
                         }
                     ]
                 }]
             }
         ],
-        additionalHeading: `<tr><td>Additional Heading</td></tr>`,
+        additionalHeading: '<tr><td>Additional Heading</td></tr>', // will be appended to <thead>, to all boxes
         labels: {
+             // labels behind hiding/showing available elements section:
             toggleAvailableOn:  'Show available elements',
             toggleAvailableOff: 'Hide available elements',
-            selectedBoxInitMsg: 'No elements selected'
+             
+            selectedBoxInitMsg: 'No elements selected' // will be displayed if no elements are selected
         },
-        bootstrapVersion: 2,
-        colWidthName:     4,
-        amountVisibleInAvailableBox: 10,
-        amountVisibleInSelectedBox:  5,
+        bootstrapVersion: 2, // will change used Bootstrap classes
+        colWidthName:     '25%', // or: '250px'
+        amountVisibleInAvailableBox: 10, // defines max-height of the available elements section
+        amountVisibleInSelectedBox:  5, // defines max-height of the selected elements section
+        // function to fire after element is moved up or down:
         callbackElementMoved: function () {
             console.log('element moved, pass data to form');
         },
+        // function to fire after all boxes have been rendered:
         callbackBoxesRendered: function () {
             console.log('boxes rendered, paint them black');
         }
--- source/js/sorti-boxes.js
+++ source/js/sorti-boxes.js
@@ -8,175 +8,58 @@
  * var options = {
         params: [
             {
-                name:  'delay',
-                label: 'Churn delay [min]'
+                name:  'foo',
+                label: 'Foo'
             },
             {
-                name:  'msisdn_unique',
-                label: 'User uniqueness [h]'
-            },
-            {
-                name:  'clickip_unique',
-                label: 'IP uniqueness [h]'
-            },
-            {
-                name:  'fire_ratio',
-                label: 'Fire ratio [%]'
-            },
-            {
-                name:  'fire_cap',
-                label: 'Fire Cap [per&nbsp;day]'
-            },
-            {
-                name:  'since_click',
-                label: 'Since click [h]'
-            },
-            {
-                name:  'since_joined',
-                label: 'Since joined [h]'
+                name:  'bar',
+                label: 'Bar'
             }
         ],
         data:   [
             {
                 id:   1,
-                name: 'Event: Joined',
+                name: 'Section Name',
 
                 boxes: [{
                     id:   1,
-                    name: 'TR', // will be used as header
+                    name: 'Box Name',
 
                     elements: [
                         {
                             id:       1,
-                            name:     'Joined - TR - MOBILE (11)',
-                            special:  true, // will add red dot
+                            name:     'Element Name',
+                            special:  true,
                             selected: true,
                             status:   true,
                             params: {
-                                // keys must match names of main params
-                                delay: `<span class="span6 val new">
-                                                <input value="2" name="triggers[19][rules][11][delay]" autocomplete="off" type="text">
-                                                </span>
-                                                <span class="span6 val old">3</span>`,
-
-                                msisdn_unique: `<span class="span6 val new">
-                                                <input value="2" name="triggers[19][rules][11][delay]" autocomplete="off" type="text">
-                                                </span>
-                                                <span class="span6 val old">3</span>`,
-
-                                clickip_unique: `<span class="span6 val new">
-                                                <input value="2" name="triggers[19][rules][11][delay]" autocomplete="off" type="text">
-                                                </span>
-                                                <span class="span6 val old">3</span>`,
-
-                                fire_ratio: `<span class="span6 val new">
-                                                <input value="2" name="triggers[19][rules][11][delay]" autocomplete="off" type="text">
-                                                </span>
-                                                <span class="span6 val old">3</span>`,
-
-                                fire_cap: `<span class="span6 val new">
-                                                <input value="2" name="triggers[19][rules][11][delay]" autocomplete="off" type="text">
-                                                </span>
-                                                <span class="span6 val old">3</span>`,
-
-                                since_click: `<span class="span6 val new">
-                                                <input value="2" name="triggers[19][rules][11][delay]" autocomplete="off" type="text">
-                                                </span>
-                                                <span class="span6 val old">3</span>`,
-
-                                since_joined: `<span class="span6 val new">
-                                                <input value="2" name="triggers[19][rules][11][delay]" autocomplete="off" type="text">
-                                                </span>
-                                                <span class="span6 val old">3</span>`,
+                                foo: 15,
+                                bar: 53
                             }
                         },
                         {
                             id:       2,
-                            name:     'Joined - TR - ONLINE (10)',
-                            special:  false, // will add red dot
+                            name:     'Element Name',
+                            special:  true,
                             selected: true,
                             status:   true,
                             params: {
-                                // keys must match names of main params
-                                delay: `<span class="span6 val new">
-                                                <input value="2" name="triggers[19][rules][11][delay]" autocomplete="off" type="text">
-                                                </span>
-                                                <span class="span6 val old">3</span>`,
-
-                                msisdn_unique: `<span class="span6 val new">
-                                                <input value="2" name="triggers[19][rules][11][delay]" autocomplete="off" type="text">
-                                                </span>
-                                                <span class="span6 val old">3</span>`,
-
-                                clickip_unique: `<span class="span6 val new">
-                                                <input value="2" name="triggers[19][rules][11][delay]" autocomplete="off" type="text">
-                                                </span>
-                                                <span class="span6 val old">3</span>`,
-
-                                fire_ratio: `<span class="span6 val new">
-                                                <input value="2" name="triggers[19][rules][11][delay]" autocomplete="off" type="text">
-                                                </span>
-                                                <span class="span6 val old">3</span>`,
-
-                                fire_cap: `<span class="span6 val new">
-                                                <input value="2" name="triggers[19][rules][11][delay]" autocomplete="off" type="text">
-                                                </span>
-                                                <span class="span6 val old">3</span>`,
-
-                                since_click: `<span class="span6 val new">
-                                                <input value="2" name="triggers[19][rules][11][delay]" autocomplete="off" type="text">
-                                                </span>
-                                                <span class="span6 val old">3</span>`,
-
-                                since_joined: `<span class="span6 val new">
-                                                <input value="2" name="triggers[19][rules][11][delay]" autocomplete="off" type="text">
-                                                </span>
-                                                <span class="span6 val old">3</span>`,
+                                foo: 67,
+                                bar: 89
                             }
-                        }
+                        },
                     ]
                 }]
             }
         ],
-        additionalHeading: `<tr class="small">
-                    <th class="span4"></th>
-                    <th class="row-fluid valInfo">
-                        <small class="span6 txt">new</small>
-                        <small class="span6 txt">default</small>
-                    </th>
-                    <th class="row-fluid valInfo">
-                        <small class="span6 txt">new</small>
-                        <small class="span6 txt">default</small>
-                    </th>
-                    <th class="row-fluid valInfo">
-                        <small class="span6 txt">new</small>
-                        <small class="span6 txt">default</small>
-                    </th>
-                    <th class="row-fluid valInfo">
-                            <small class="span6 txt">new</small>
-                            <small class="span6 txt">default</small>
-                        </th>
-                    <th class="row-fluid valInfo">
-                        <small class="span6 txt">new</small>
-                        <small class="span6 txt">default</small>
-                    </th>
-                    <th class="row-fluid valInfo">
-                        <small class="span6 txt">new</small>
-                        <small class="span6 txt">default</small>
-                    </th>
-                    <th class="row-fluid valInfo">
-                        <small class="span6 txt">new</small>
-                        <small class="span6 txt">default</small>
-                    </th>
-                    <th class="span1"> </th>
-                </tr>`,
+        additionalHeading: `<tr><td>Additional Heading</td></tr>`,
         labels: {
             toggleAvailableOn:  'Show available elements',
             toggleAvailableOff: 'Hide available elements',
             selectedBoxInitMsg: 'No elements selected'
         },
         bootstrapVersion: 2,
-        colWidthName:     4,
+        colWidthName:     '25%',
         amountVisibleInAvailableBox: 10,
         amountVisibleInSelectedBox:  5,
         callbackElementMoved: function () {
@@ -559,7 +442,6 @@ require('../themes/default.pcss');
             }
         };
 
-
         return {
             adjustContainerHeight,
             moveToAvailable,
@@ -629,7 +511,6 @@ require('../themes/default.pcss');
                 .text(boxAvailableToggle.hasClass('sorti-box-available-toggle-collapsed') ?
                     options.labels.toggleAvailableOn : options.labels.toggleAvailableOff);
         };
-
 
         return {
             adjustContainerHeight,
